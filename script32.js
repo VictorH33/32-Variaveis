3a22b51f898860fb9ca92b8fbf6bcd04d144ab1d
Victor Hugo <victor33hsribeiro@gmail.com>
2024-11-26 16:07:28 -0300

    Add files via upload

---
+++ script32.js
@@ -0,0 +1,9 @@
+let x;
+let y;
+
+x = 5;
+y = 6;
+let z = x + y;
+
+// Números com decimais, devem ser escritos com . ao invés de ,
+// Número na casa de mil não necessita de . (ex: 1.000 X | 1000 V )
